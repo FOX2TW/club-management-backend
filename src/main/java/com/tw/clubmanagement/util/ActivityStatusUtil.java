2860b05491d9b273c35f36e28b54a65adb4fcf1b
jun.li <lijun359714923@163.com>
2020-03-11 12:12:11 +0800

    add log

--- src/main/java/com/tw/clubmanagement/util/ActivityStatusUtil.java
+++ src/main/java/com/tw/clubmanagement/util/ActivityStatusUtil.java
@@ -1,13 +1,17 @@
 package com.tw.clubmanagement.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Date;
 
+@Slf4j
 public class ActivityStatusUtil {
 
     public static Integer calculateStatus(Date joinEndTime, Date start, Date end, Integer numberLimitation, Integer numberJoined) {
         boolean isMatchedNumberLimitation = numberJoined.intValue() == numberLimitation.intValue();
 
         Date now = new Date();
+        log.info("now: {}, join_end: {}, start: {}, end: {}, numberLimitation: {}, numberJoined: {}", now, joinEndTime, start, end, numberLimitation, numberLimitation);
         if (joinEndTime != null && now.before(joinEndTime) && !isMatchedNumberLimitation) {
             return 0;
         }
